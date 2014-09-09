77b3f6c68920b579f6edc28da3bfa8438c4087d8
bribzy@gmail.com <bribzy@gmail.com>
2014-09-09 17:27:22 +0800

    add demo

---
+++ 101/Slideshow/src/data/SlideData.js
@@ -0,0 +1,12 @@
+define(function(require, exports, module) {
+    var SlideData = {
+    };
+
+    SlideData.parse = function(data) {
+        var urls = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg',
+        'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg'];
+        return urls;
+    };
+
+    module.exports = SlideData;
+});
